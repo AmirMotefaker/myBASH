d5444173b4a32a659e1f5b3f68aaa5c28c0b9124
Amir Motefaker <amotef@gmail.com>
2022-07-05 11:12:13 +0430

    Create calculator_intermediate.sh

---
+++ calculator_intermediate.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# Calculator Intermediate
+
+if [ $2 == "+" ]; then
+    echo "addition of your two numbers are $(($1 + $3))"
+elif [ $2 == "-" ]; then
+    echo "subtraction of your two numbers are $(($1 - $3))"
+elif [ $2 == "x" ]; then
+    echo "multiplication of your two numbers are $(($1 * $3))"
+else
+    echo "division of your two numbers are $(($1 / $3))"
+fi
