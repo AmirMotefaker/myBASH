c6070921d1dfba42c245c7c13697c784d6d4f257
Amir Motefaker <amotef@gmail.com>
2022-07-05 12:32:07 +0430

    Create weather.sh

---
+++ weather.sh
@@ -0,0 +1,19 @@
+# Code by @AmirMotefaker
+
+# Weather
+
+#!/bin/bash
+
+case $1 in
+-h | --help)
+    echo "Short option     Long option     Description"
+    echo "-h               --help          show help"
+    echo "-l               --location      show weather"
+    ;;
+-l | --location)
+    curl https://wttr.in/$2
+    ;;
+*)
+    curl https://wttr.in/
+    ;;
+esac
