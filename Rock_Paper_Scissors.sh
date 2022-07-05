3268d40a20ed4663c3c8e1f9f877d13a2f03476f
Amir Motefaker <amotef@gmail.com>
2022-07-05 11:08:11 +0430

    Create Rock_Paper_Scissors.sh

---
+++ Rock_Paper_Scissors.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# Rock Paper Scissors - Basic
+
+if [ $1 == "sang" ]; then
+        echo "$1 Will Lose kaqaz"
+        echo "$1 Will Draw sang"
+        echo "$1 Will Win gheichi"
+elif [ $1 == "gheichi" ]; then
+        echo "$1 Will Lose sang"
+        echo "$1 Will Draw gheichi"
+        echo "$1 Will Win kaqaz"
+elif [ $1 == "kaqaz" ]; then
+        echo "$1 Will Lose gheichi"
+        echo "$1 Will Draw kaqaz"
+        echo "$1 Will Win sang"
+fi
