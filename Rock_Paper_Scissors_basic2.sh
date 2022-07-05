e98e0bf28c90e2b7bed256a76f2b657a3de75c2c
Amir Motefaker <amotef@gmail.com>
2022-07-05 11:10:25 +0430

    Create  Rock_Paper_Scissors_basic2.sh

---
+++ Rock_Paper_Scissors_basic2.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# Rock Paper Scissors - Basic2
+
+declare -A win lose draw
+
+win=([sang]=gheichi [kaqaz]=sang [gheichi]=kaqaz)
+draw=([sang]=sang [kaqaz]=kaqaz [gheichi]=gheichi)
+lose=([sang]=kaqaz [kaqaz]=gheichi [gheichi]=sang)
+
+echo "$1 Will Lose ${lose[$1]}"
+echo "$1 Will Draw ${draw[$1]}"
+echo "$1 Will Win ${win[$1]}"
