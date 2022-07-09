d027c677e5334024750142a6bd664fe37afc7b29
Amir Motefaker <amotef@gmail.com>
2022-07-09 12:41:16 +0430

    Update todo.sh
    Added priority

--- todo.sh
+++ todo.sh
@@ -4,15 +4,24 @@ case $1 in
 "add")
   shift
   priority='L'
-  case "$1" in
-  -t | --title) mode=1 ;;
-  -p | --priority) mode=2 ;;
-  *) ;;
-  esac
-  if [ "${mode}" == 1 ]; then
-    title=$2
-    shift 2
-  fi
-  echo "0,$priority,\"$title\"" >> tasks.csv
+  while [ -n "$1" ]; do
+    case "$1" in
+    -t | --title) mode=1 ;;
+    -p | --priority) mode=2 ;;
+    *) ;;
+    esac
+    if [ "${mode}" == 1 ]; then
+      if [ -z "$2" ]; then
+        echo "Option -t|--title Needs a Parameter"
+        exit
+      fi
+      title=$2
+      shift 2
+    elif [ "${mode}" == 2 ]; then
+      priority=$2
+      shift 2
+    fi
+  done
+  echo "0,$priority,\"$title\"" >>tasks.csv
   ;;
 esac
