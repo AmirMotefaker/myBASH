7c4679787c623f1aa89801eb5cfc813e7c11a339
Amir Motefaker <amotef@gmail.com>
2022-07-11 16:54:46 +0430

    Update calculator_advanced2.sh
    Adde full code but an error in subtraction  and division

--- calculator_advanced2.sh
+++ calculator_advanced2.sh
@@ -12,3 +12,39 @@ case $1 in
         echo "addition of your two numbers are $sum"
     ;;
 esac
+case $1  in
+    -)  shift
+        sub=$1
+        shift
+        while [ $1 ]
+        do
+            sub=$(($1 - sub))
+            shift
+        done
+        echo "subtraction of your two numbers are $sub"
+    ;;
+esac
+case $1  in
+    x)  shift
+        mult=$1
+        shift
+        while [ $1 ]
+        do
+            mult=$(($1 * mult))
+            shift
+        done
+        echo "multiplication of your two numbers are $mult"
+    ;;
+esac
+case $1  in
+    /)  shift
+        dev=$1
+        shift
+        while [ $1 ]
+        do
+            dev=$(($1 / dev))
+            shift
+        done
+        echo "division of your two numbers are $dev"
+    ;;
+esac
