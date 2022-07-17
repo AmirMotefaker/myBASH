e4307170bae5b7b285d4eba5210364d5cd3bee9b
Amir Motefaker <amotef@gmail.com>
2022-07-17 10:30:51 +0430

    Update todo.sh
    Complete code

--- todo.sh
+++ todo.sh
@@ -1,40 +1,55 @@
-#!/bin/bash
+# Code by @AmirMotefaker
 
-case $1 in
-"add")
-  shift
-  priority='L'
-  while [ -n "$1" ]; do
-    case "$1" in
-    -t | --title) mode=1 ;;
-    -p | --priority) mode=2 ;;
-    *) ;;
-    esac
-    if [ "${mode}" == 1 ]; then
-      if [ -z "$2" ]; then
-        echo "Option -t|--title Needs a Parameter"
-        exit
-      fi
-      title=$2
-      shift 2
-    elif [ "${mode}" == 2 ]; then
-      if [[ $2 != 'L' && $2 != 'M' && $2 != 'H' ]]; then
-        echo "Option -p|--priority Only Accept L|M|H"
-        exit
-      fi
-      priority=$2
-      shift 2
-    fi
-  done
-  echo "0,$priority,\"$title\"" >>tasks.csv
-  ;;
-esac
-
+# ToDo List
 
 #! /bin/bash
 
 # Tasks File
 file=tasks.csv
+
+# Add Task to Tasks File
+function _add {
+  # Task Title Should be in " " because of some Special Characters CSV
+  echo "0,$1,\"$2\"" >> "$file"
+  echo "Task '$2' Added to List."
+}
+
+case $1 in
+"add")
+  while [ -n "$2" ]
+  do
+    case "$2" in
+      -t | --title)
+        if [ -z "$3" ]; then
+           echo "Option -t|--title Needs a Parameter"
+           exit
+        fi
+        name="$3"
+        shift
+        shift ;;
+
+      -p | --priority)
+        priority=$3
+        if [[ $priority != 'L' && $priority != 'M' && $priority != 'H' ]]; then
+          echo "Option -p|--priority Only Accept L|M|H"
+          exit
+        fi
+        shift
+        shift ;;
+
+      *)
+        echo "Invalid Option"
+        exit ;;
+    esac
+  done
+  # Default Task Priority
+  if [[ -z "$priority" ]]; then
+    priority="L"
+  fi
+  _add "$priority" "$name";;
+*)
+  echo "Command Not Supported!";;
+esac
 
 # Get Tasks from Tasks File and List Them
 function _list {
